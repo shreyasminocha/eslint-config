7f24d88c5330f1595d694073fea2ad6e73df51f2
Shreyas Minocha <shreyasminocha@protonmail.com>
2019-01-10 17:28:38 +0530

    Sort rules by name

--- index.js
+++ index.js
@@ -8,47 +8,49 @@ const config = {
 
     extends: [
         'airbnb-base',
-        'plugin:unicorn/recommended',
         'plugin:eslint-comments/recommended',
+        'plugin:unicorn/recommended',
         './browser.js',
         './node.js'
     ],
 
     rules: {
+        'array-bracket-newline': ['error', 'consistent'],
+        'arrow-body-style': 'off',
+        'capitalized-comments': ['error', 'always'],
+        'comma-dangle': ['error', 'never'],
+        'func-names': 'off',
+        'func-style': ['error', 'declaration'],
         indent: [
             'error', 4,
             { SwitchCase: 0 }
         ],
-        'no-use-before-define': 'off',
-        'no-plusplus': 'off',
-        'arrow-body-style': 'off',
-        'no-param-reassign': 'off',
-        'no-self-assign': ['error', {
-            props: true
-        }],
-        strict: ['error', 'global'],
-        'array-bracket-newline': ['error', 'consistent'],
-        'func-names': 'off',
-        'func-style': ['error', 'declaration'],
-        'capitalized-comments': ['error', 'always'],
         'line-comment-position': 'off',
-        'max-len': ['warn', 80, {
-            tabWidth: 4,
-            ignoreUrls: true,
-            ignoreStrings: true,
-            ignoreRegExpLiterals: true
-        }],
+        'max-len': [
+            'warn', 80, {
+                tabWidth: 4,
+                ignoreStrings: true,
+                ignoreRegExpLiterals: true,
+                ignoreUrls: true
+            }
+        ],
         'multiline-ternary': 'off',
+        'no-continue': 'off',
+        'no-param-reassign': 'off',
+        'no-plusplus': 'off',
         'no-restricted-syntax': [
             'error',
             'ForInStatement', 'LabeledStatement', 'WithStatement'
         ],
+        'no-self-assign': ['error', {
+            props: true
+        }],
         'no-ternary': 'warn',
         'no-underscore-dangle': ['warn', {
             allowAfterThis: true
         }],
-        'no-continue': 'off',
-        'comma-dangle': ['error', 'never'],
+        'no-use-before-define': 'off',
+        strict: ['error', 'global'],
         'unicorn/no-process-exit': 'off' // Duplicate of no-process-exit
     }
 };
